85a8379e5b9e9a58bfe814255e436f4a0181eabe
TheVirtualRealm <70163600+TheVirtualRealm@users.noreply.github.com>
2021-04-14 12:59:46 -0400

    Added in Enimies and there stats

---
+++ jump.js
@@ -0,0 +1,55 @@
+var enemies = [
+
+
+    {
+        'name': 'Arachna',
+        'type': 'Spider',
+        'ability': 'Toxic Bite',
+        'health': 350,
+        'power': 15,
+        'dodge': 6,
+        'block': 35,
+    },
+
+    {
+        'name': 'Crimson',
+        'type': 'Pyromaniac',
+        'ability': 'FlameThrower',
+        'health': 600,
+        'power': 30,
+        'dodge': 10,
+        'block': 20,
+    },
+
+    {
+        'name': 'Damien',
+        'type': 'Schizophrenic',
+        'ability': 'The Whispers',
+        'health': 250,
+        'power': 85,
+        'dodge': 70,
+        'block': 55,
+    },
+
+    {
+        'name': 'Midnight',
+        'type': 'Dissociative',
+        'ability': 'Memory loss',
+        'health': 100,
+        'power': 100,
+        'dodge': 95,
+        'block': 85,
+    },
+
+    {
+        'name': 'Lilly',
+        'type': 'Insomniac',
+        'ability': 'Deathly Rage',
+        'health': 215,
+        'power': 25,
+        'dodge': 5,
+        'block': 3,
+    },
+
+
+]
